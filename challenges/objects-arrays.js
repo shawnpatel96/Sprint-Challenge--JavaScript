2c515f9bb575ffc79db3316d8e8492ca465a0737
shawn <shawnpatel196@gmail.com>
2019-12-20 11:01:14 -0600

    finished object page

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -23,7 +23,7 @@ let tyrannosaurus = {
 }
 
 
-console.log(tyrannosaurus.roar)
+
 console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
@@ -49,10 +49,10 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-tyrannosaurus.roar = function(){
+tyrannosaurus.roar = ()=>{
   return `ROARRRRRRRRRR`
 }
-tyrannosaurus.roar();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -75,7 +75,16 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+graduates.sort(function(a,b){
+  let textA= a.university.toUpperCase();
+  let textB= b.university.toUpperCase();
+  return(textA <textB) ? -1 :(textA > textB) ? 1 : 0;
+})
 const universities = [];
+const schools = graduates.map((college)=>{
+  return `${college.university}`
+})
+universities.push(schools);
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -85,10 +94,19 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+const studentInfo = graduates.map((info)=>{
+  return `${info.first_name}, ${info.email}`
+})
+contactInfo.push(studentInfo);
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+console.log(unisWithUni)
+for(let i=0; i < graduates.length; i++){
+  unisWithUni.push(graduates[i].university.match('Uni'))
+  // console.log(unisWithUni[i])
+}
 console.log(unisWithUni);
 
 
@@ -115,6 +133,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach((animals)=>{
+  displayNames.push(`Name: ${animals.animal_name}, Scientific: ${animals.scientific_name}.`)
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -124,7 +145,13 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+const lowerCase = zooAnimals.map((item)=>{
+  item.animal_name = item.animal_name.toLowerCase();
+  return item
+})
+lowCaseAnimalNames.push(lowerCase);
 console.log(lowCaseAnimalNames);
+
 
 /* Request 3: .filter() 
 
@@ -132,6 +159,12 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+const low =zooAnimals.filter((lowpop)=>{
+  if(lowpop.population < 5)      // Are we including animals with a population of 5 as well? if so add = sign to condition  // lowpop.population <=5
+  return true;
+
+})
+lowPopulationAnimals.push(low)
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -139,8 +172,12 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+// const populationTotal = 0;
+
+const popSum = zooAnimals.reduce(function(total, pop){
+  return total + pop.population;
+}, 0);
+console.log(popSum);
 
 
 /*
@@ -148,4 +185,5 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
+// CONVERTED ALL CALL BACK FUNCTION TO ARROW
 
